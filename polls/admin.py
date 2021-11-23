66f57b37b65ba3b59e083a2f0bb5f289ef770e84
Alvin Wanjohi <akwanjohi99@gmail.com>
2021-11-23 08:31:11 +0300

    Registered new models in admin.
    - Modified user creation and question creation forms in admin.

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Question, Topic, Choice
 
-admin.site.register(User)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['creator', 'question_text', 'topic']}), ]
+    inlines = [ChoiceInline]
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                'first_name', 'last_name', 'username', 'email', 'password'
+            ),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Topic)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
